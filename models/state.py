55e356a770ac5f68389e0b315f18b5195ed4cf90
Karisma-471 <okoyecharisma@gmail.com>
2024-01-02 02:34:25 +0100

    Adding state element to my models

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" This is the State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models import storage_type
 from models.city import City
