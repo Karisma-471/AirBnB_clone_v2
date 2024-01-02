cb8197c86ed632e3d505e1c625d4fd2feae7223d
Karisma-471 <okoyecharisma@gmail.com>
2024-01-02 02:37:55 +0100

    Adding my unit test module

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A unit test module for the console (command interpreter).
+"""This is a unit test module for the console (command interpreter).
 """
 import json
 import MySQLdb
